c3325504cd1e349836a78b9eaed4bee366657211
Richie-Yang <richieyang8813@gmail.com>
2022-01-18 11:58:56 +0800

    feat: add authenication logic in some routes

---
+++ middleware/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (!req.isAuthenticated()) return res.redirect('/users/login')
+    return next()
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,17 @@ const router = express.Router()
 const passport = require('passport')
 const userController = require('../controllers/userController')
 const todoController = require('../controllers/todoController')
+const { authenticator } = require('../middleware/auth')
 
 
 router.get('/users/login', userController.loginPage)
 router.post('/users/login', passport.authenticate('local', { failureRedirect: '/users/login' }), userController.login)
 router.get('/users/register', userController.registerPage)
 router.post('/users/register', userController.register)
-router.get('/users/logout', userController.logout)
+router.get('/users/logout', authenticator, userController.logout)
 
-router.get('/todos/:id', todoController.getTodo)
-router.get('/', todoController.getTodos)
+router.get('/todos/:id', authenticator, todoController.getTodo)
+router.get('/', authenticator, todoController.getTodos)
 
 
 module.exports = router
