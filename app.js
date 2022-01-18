c28d24370784171429445318decdf73327289532
Richie-Yang <richieyang8813@gmail.com>
2022-01-18 10:52:37 +0800

    feat: add /todos/:id routing, function and view page

--- app.js
+++ app.js
@@ -39,16 +39,35 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  return User.create({
-    name, email, password
-  })
-    .then(user => res.redirect('/'))
+
+  return User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('User already exists')
+        return res.render('register', { name, email })
+      }
+
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(salt, password))
+        .then(hash => {
+          return User.create({ name, email, password: hash })
+        })
+        .then(() => res.redirect('/users/login'))
+    })
+    .catch(err => console.log(err))
 })
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
 })
 
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params
+
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(err => console.log(err))
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://127.0.0.1:${PORT}`)
